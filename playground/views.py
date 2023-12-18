13159b1ced80d3ea3257b94ac2017700f3f3deda
1613-kg <kushagragarg13@gmail.com>
2023-12-18 15:30:56 +0530

    celery broker

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from .tasks import notify_customer
 
 def say_hello(request):
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    notify_customer.delay('Hello')
+    return render(request, 'hello.html', {'name': 'Kushagra'})
+     
--- storefront/settings.py
+++ storefront/settings.py
@@ -182,3 +182,6 @@ EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 2525
 DEFAULT_FROM_EMAIL = 'kushagragarg13@gmail.com'
+
+
+CELERY_BROKER_URL = 'redis://localhost:6379/1'
